6c33b5d8c0aa661151c4bbb6205980a8ac3d7bca
yuribodo <yemotalara@gmail.com>
2024-11-27 22:14:05 -0400

    feat: improve modal design to align with neetcode

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,28 +1,36 @@
 'use client';
 
-interface ModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    nodeData: { id: string; label: string } | null;
-  }
-  
+import { motion } from 'framer-motion';
 
-  export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, nodeData }) => {
-    if (!isOpen || !nodeData) return null;
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  nodeData: { id: string; label: string } | null;
+}
+
+export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, nodeData }) => {
+  if (!isOpen || !nodeData) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded shadow-lg w-1/3">
-        <h2 className="text-lg font-bold mb-4">Node Details</h2>
-        <p><strong>Node ID:</strong> {nodeData.id}</p>
-        <p><strong>Label:</strong> {nodeData.label}</p>
+    <div className="fixed inset-0 bg-black/50 flex justify-end z-50">
+      <motion.div
+        initial={{ x: '100%' }}
+        animate={{ x: 0 }}
+        exit={{ x: '100%' }}
+        transition={{ duration: 0.4, ease: 'easeInOut' }}
+        className="bg-gray-800 h-full w-4/5 p-4 shadow-lg"
+      >
         <button
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          className="bg-red-100 font-bold text-xs text-red-600 px-4 py-2 rounded hover:bg-red-200"
           onClick={onClose}
         >
-          Close
+          ESC
         </button>
-      </div>
+
+        <div className="text-white flex flex-col items-center ">
+          <h2 className="text-xl font-bold mb-4">{nodeData.label}</h2>
+        </div>
+      </motion.div>
     </div>
   );
-}
+};
