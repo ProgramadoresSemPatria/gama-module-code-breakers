46bbd3803012a9709dadbccf75dbeba2cb73141e
yuribodo <yemotalara@gmail.com>
2024-11-27 21:59:42 -0400

    feat: show modal when node cliecked

--- app/components/Flow.tsx
+++ app/components/Flow.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { Controls, ReactFlow, ReactFlowProvider, useNodesState } from '@xyflow/react';
+import { Controls, ReactFlow, ReactFlowProvider, useNodesState, Node } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import { useState } from 'react';
+import { Modal } from './Modal';
 
-const initialNodes = [
+const initialNodes: Node[] = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
 ];
@@ -14,12 +16,24 @@ const initialEdges = [
 
 export function Flow() {
   const [nodes, _setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [selectedNode, setSelectedNode] = useState<Node | null>(null); 
+  const [isModalOpen, setModalOpen] = useState<boolean>(false); 
+
+  const handleNodeClick = (_event: React.MouseEvent, node: Node) => {
+    setSelectedNode(node);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedNode(null);
+  };
 
 
   return (
     <div className='h-full w-full relative p-4'>
       <ReactFlowProvider>
-        <ReactFlow edges={initialEdges} nodes={nodes} onNodesChange={onNodesChange} />
+        <ReactFlow edges={initialEdges} nodes={nodes} onNodesChange={onNodesChange}  onNodeClick={handleNodeClick}/>
 
         <Controls
           position='bottom-right'
@@ -28,6 +42,16 @@ export function Flow() {
           className='bg-slate-300 rounded hover:bg-slate-400 transition-all'
         />
       </ReactFlowProvider>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        nodeData={
+          selectedNode && typeof selectedNode.data.label === 'string'
+            ? { id: selectedNode.id, label: selectedNode.data.label }
+            : null
+        }
+      />
     </div>
   );
 }
