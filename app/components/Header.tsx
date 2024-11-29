0f9ab668b2d49a617e8c7fe5c11a1a6280532371
yuribodo <yemotalara@gmail.com>
2024-11-28 20:37:50 -0400

    feat: create header component initial design

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,55 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Sun, Moon, Flame } from 'lucide-react';
+import Image from 'next/image';
+
 export function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(false); 
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
   return (
-    <header className='bg-gray-800 text-white p-4 h-12'>
-      <h1 className='text-xl'>Header</h1>
+    <header className="bg-gray-800 text-white p-4 h-14 flex justify-between items-center">
+      
+      <div className="flex items-center space-x-8 cursor-pointer ml-4 font-semibold">
+        <div>
+          <Image src="/images/logo-borderless.webp" alt="logo" width={24} height={40} />
+        </div>
+        <div>
+          <span>Courses</span>
+        </div>
+        <div>
+          <span>Practices</span>
+        </div>
+        <div>
+          <span>Roadmap</span>
+        </div>
+        <div>
+          <span>Newsletter</span>
+        </div>
+      </div>
+
+      
+      <div className="flex items-center space-x-4 cursor-pointer">
+        
+        <div className="flex items-center space-x-2 bg-[#1d9772] px-4 py-1 rounded-xl font-semibold">
+          <Flame size={20} strokeWidth={3} />
+          <span>Pro</span>
+        </div>
+
+       
+        <div onClick={toggleTheme} className="cursor-pointer hover:bg-black px-4 py-2 rounded-2xl">
+          {isDarkMode ? <Moon size={20} /> : <Sun size={20} />}
+        </div>
+
+        
+        <div className="flex justify-center items-center space-x-2 bg-[#4814b0] px-4 py-1 text-white rounded-md font-semibold">
+          <span>Sign in</span>
+        </div>
+      </div>
     </header>
   );
 }
