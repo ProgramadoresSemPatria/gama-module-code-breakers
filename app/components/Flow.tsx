5ab5a10cd22044c4e7b18e330fb9b81154b88857
yuribodo <yemotalara@gmail.com>
2024-11-29 23:34:03 -0400

    feat: improve nodes design

--- app/components/Flow.tsx
+++ app/components/Flow.tsx
@@ -5,25 +5,27 @@ import '@xyflow/react/dist/style.css';
 import { useState } from 'react';
 import { Modal } from './Modal';
 
+
+
 const initialNodes: Node[] = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: 'Arrays & Hashing' } },
-  { id: '2', position: { x: -100, y: 130 }, data: { label: 'Two Pointers' } },
-  { id: '3', position: { x: 100, y: 130 }, data: { label: 'Stack' } },
-  { id: '4', position: { x: -200, y: 230 }, data: { label: 'Binary Search' } },
-  { id: '5', position: { x: 0, y: 230 }, data: { label: 'Sliding Window' } },
-  { id: '6', position: { x: 200, y: 230 }, data: { label: 'Linked List' } },
-  { id: '7', position: { x: -100, y: 330 }, data: { label: 'Trees' } },
-  { id: '8', position: { x: -350, y: 430 }, data: { label: 'Tries' } },
-  { id: '9', position: { x: 300, y: 430 }, data: { label: 'Backtracking' } },
-  { id: '10', position: { x: -200, y: 530 }, data: { label: 'Heap/ Priority Queue' } },
-  { id: '11', position: { x: 200, y: 530 }, data: { label: 'Graphs' } },
-  { id: '12', position: { x: 500, y: 530 }, data: { label: '1-D DP' } },
-  { id: '13', position: { x: -330, y: 630 }, data: { label: 'Intervals' } },
-  { id: '14', position: { x: -130, y: 630 }, data: { label: 'Greedy' } },
-  { id: '15', position: { x: 70, y: 630 }, data: { label: 'Advanced Graphs' } },
-  { id: '16', position: { x: 380, y: 630 }, data: { label: '2-D DP' } },
-  { id: '17', position: { x: 560, y: 630 }, data: { label: 'Bit Manipulation' } },
-  { id: '18', position: { x: 330, y: 730 }, data: { label: 'Math & Geometry' } },
+  { id: '1', position: { x: 0, y: 0 }, data: { label: 'Arrays & Hashing' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '2', position: { x: -100, y: 130 }, data: { label: 'Two Pointers' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '3', position: { x: 100, y: 130 }, data: { label: 'Stack' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '4', position: { x: -200, y: 230 }, data: { label: 'Binary Search' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '5', position: { x: 0, y: 230 }, data: { label: 'Sliding Window' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '6', position: { x: 200, y: 230 }, data: { label: 'Linked List' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '7', position: { x: -100, y: 330 }, data: { label: 'Trees' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '8', position: { x: -350, y: 430 }, data: { label: 'Tries' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '9', position: { x: 300, y: 430 }, data: { label: 'Backtracking' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '10', position: { x: -200, y: 530 }, data: { label: 'Heap/ Priority Queue' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '11', position: { x: 200, y: 530 }, data: { label: 'Graphs' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '12', position: { x: 500, y: 530 }, data: { label: '1-D DP' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '13', position: { x: -330, y: 630 }, data: { label: 'Intervals' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '14', position: { x: -130, y: 630 }, data: { label: 'Greedy' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '15', position: { x: 70, y: 630 }, data: { label: 'Advanced Graphs' } , style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'}},
+  { id: '16', position: { x: 380, y: 630 }, data: { label: '2-D DP' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '17', position: { x: 560, y: 630 }, data: { label: 'Bit Manipulation' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
+  { id: '18', position: { x: 330, y: 730 }, data: { label: 'Math & Geometry' }, style: { backgroundColor: '#454d9e', color: '#fff', fontWeight: 'bold'} },
 ];
 
 const initialEdges = [
@@ -281,6 +283,12 @@ const initialEdges = [
   },
 ];
 
+const styles = {
+  backgroundColor: '#13181c',
+  width: '100%',
+  height: 300,
+};
+
 export function Flow() {
   const [nodes, _setNodes, onNodesChange] = useNodesState(initialNodes);
   const [selectedNode, setSelectedNode] = useState<Node | null>(null); 
@@ -305,7 +313,7 @@ export function Flow() {
     <div className='h-full w-full relative p-4'>
       <ReactFlowProvider>
         
-        <ReactFlow edges={initialEdges} nodes={nodes} onNodesChange={onNodesChange} style={{ backgroundColor: '#13181c' }} fitView  onNodeClick={handleNodeClick} />
+        <ReactFlow  edges={initialEdges} nodes={nodes} onNodesChange={onNodesChange} style={styles} fitView  onNodeClick={handleNodeClick} />
 
 
         <Controls
